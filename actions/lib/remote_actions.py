5c99112e182bb53a05cb9496f0d41d65c429ed39
Nick Maludy <nmaludy@gmail.com>
2018-08-06 15:37:26 -0400

    Fix problem with command not found

--- actions/lib/remote_actions.py
+++ actions/lib/remote_actions.py
@@ -10,19 +10,25 @@ __all__ = [
 class PuppetBaseAction(object):
     PUPPET_BINARY = 'puppet'
 
-    def _run_command(self, cmd):
+    def _run_command(self, cmd, env=None, cwd=None, handle_result=True):
         cmd_string = ' '.join(pipes.quote(s) for s in cmd)
         sys.stderr.write('Running command "%s"\n' % (cmd_string))
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
+                                   stderr=subprocess.PIPE,
+                                   env=env,
+                                   cwd=cwd)
         stdout, stderr = process.communicate()
         exit_code = process.returncode
+        if handle_result:
+            return self._handle_command_result(exit_code=exit_code, stdout=stdout,
+                                               stderr=stderr)
+        else:
+            return exit_code, stdout, stderr
 
-        return self._handle_command_result(exit_code=exit_code, stdout=stdout,
-                                           stderr=stderr)
-
-    def _get_full_command(self, args):
-        cmd = [self.PUPPET_BINARY] + args
+    def _get_full_command(self, args, puppet_cmd=None):
+        if not puppet_cmd:
+            puppet_cmd = self.PUPPET_BINARY
+        cmd = [puppet_cmd] + args
         return cmd
 
     def _handle_command_result(self, exit_code, stdout, stderr):
--- actions/lookup.py
+++ actions/lookup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
+import json
+import os
+import six
+import sys
+
 
 from lib.remote_actions import PuppetBaseAction
 
@@ -11,15 +16,17 @@ __all__ = [
 
 class PuppetLookupAction(PuppetBaseAction):
 
-    def run(self, config=None, explain=False, node=None,
+    def run(self, env=None, cwd=None, puppet_cmd=None, settings=None, explain=False, node=None,
             facts=None, environment=None, merge=None,
             knock_out_prefix=None, sort_merged_arrays=False, merge_hash_arrays=False,
             explain_options=False, default=None, type=None, compile=False,
-            keys=None):
+            render_as=None, keys=None):
         args = ['lookup']
 
-        if config:
-            args += ['--config={}'.format(config)]
+        if settings:
+            s = eval(settings)
+            for k, v in six.iteritems(s):
+                args += ['--{}={}'.format(k, v)]
         if explain:
             args += ['--explain']
         if node:
@@ -44,16 +51,43 @@ class PuppetLookupAction(PuppetBaseAction):
             args += ['--type={}'.format(type)]
         if compile:
             args += ['--compile']
+        if render_as:
+            args += ['--render-as={}'.format(render_as)]
 
         # run one command per key
-        for k in keys:
-            cmd = self._get_full_command(args=args + [k])
-            self._run_command(cmd=cmd)
+        results = {}
+        keys_list = eval(keys)
+        for key in keys_list:
+            cmd = self._get_full_command(args=(args + [key]),
+                                         puppet_cmd=puppet_cmd)
+            exit_code, stdout, stderr = self._run_command(cmd=cmd,
+                                                          env=env,
+                                                          cwd=cwd,
+                                                          handle_result=False)
+            success = False if exit_code else True
+            if not success:
+                sys.stdout.write(stdout)
+                sys.stderr.write(stderr)
+                exit(exit_code)
+
+            if stdout:
+                if render_as == 'json':
+                    results[key] = json.loads(stdout)
+                else:
+                    results[key] = stdout
+            if stderr:
+                sys.stderr.write(stderr)
+
+        if len(keys_list) > 1:
+            sys.stdout.write(json.dumps(results))
+        else:
+            sys.stdout.write(json.dumps(results[keys_list[0]]))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run puppet lookup')
-    parser.add_argument('--config', required=False)
+    parser.add_argument('--puppet_cmd', required=False)
+    parser.add_argument('--settings', required=False)
     parser.add_argument('--explain', action='store_true', default=False, required=False)
     parser.add_argument('--node', required=False)
     parser.add_argument('--facts', required=False)
@@ -70,8 +104,12 @@ if __name__ == '__main__':
     parser.add_argument('--type', required=False)
     parser.add_argument('--compile', action='store_true', default=False,
                         required=False)
-    parser.add_argument('keys', nargs='+')
+    parser.add_argument('--render_as', required=False)
+    parser.add_argument('keys')
 
     args = vars(parser.parse_args())
-    action = PuppetRunAgentAction()
+    action = PuppetLookupAction()
+    # inherit environment and cwd from the runner
+    args['env'] = os.environ.copy()
+    args['cwd'] = os.getcwd()
     action.run(**args)
